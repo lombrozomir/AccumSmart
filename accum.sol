169c7ff227d9bbc69854f52b01f812c129403d49
lombrozomir <roman.ryzhkov31@gmail.com>
2021-10-17 17:01:56 +0300

    first commit

---
+++ accum.sol
@@ -0,0 +1,33 @@
+pragma ton-solidity >= 0.35.0;
+pragma AbiHeader expire;
+
+contract Accumulator {
+
+	// State variable storing the sum of arguments that were passed to function 'add',
+	uint public sum = 1;
+
+	constructor() public {
+		// check that contract's public key is set
+		require(tvm.pubkey() != 0, 101);
+		// Check that message has signature (msg.pubkey() is not zero) and message is signed with the owner's private key
+		require(msg.pubkey() == tvm.pubkey(), 102);
+		tvm.accept();
+	}
+
+	// Modifier that allows to accept some external messages
+	modifier checkOwnerAndAccept {
+		// Check that message was signed with contracts key.
+		require(msg.pubkey() == tvm.pubkey(), 102);
+		tvm.accept();
+		_;
+	}
+
+	// Function that adds its argument to the state variable.
+	function add(uint value) public checkOwnerAndAccept {
+		sum += value;
+	}
+    function multiply(uint value) public checkOwnerAndAccept {
+        require(0 < value && value < 11, 228);
+        sum *= value;
+	}
+}
